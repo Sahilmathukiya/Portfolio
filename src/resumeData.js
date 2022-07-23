4d30480841759f873755652453780abaa8fe2096
Sahilmathukiya <sahilmathukiya999@gmail.com>
2022-07-23 11:32:30 +0530

    Intial comit

---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+    "imagebaseurl":"https://sahilmathukiya.github.io/",
+    "name": "Sahil Mathukiya",
+    "role": "Frontend Developer and Business Analyst Developer",
+    "linkedinId":"sahil mathukiya",
+    "skypeid": "sahil mathukiya",
+    "roleDescription": "To obtain a creative and challenging position in an organization that gives me an opportunity for self improvement and leadership, while contributing to the symbolic growth of the organization with my technical, innovative and logical skills.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mathukiya-sahil-322714194/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/sahilmathukiya",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/mathukiyasahil1",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently studying an undergraduate course of computer engineering at V.V.P. engineering college. I have good technical, analytical and management skills and I am also good in working with team.",
+    "address":"India",
+    "website":"https://sahilmathukiya.github.io",
+    "education":[
+      {
+        "UniversityName":"Gujarat Technological University",
+        "specialization":"Coputer Engieering",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2023",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    
+  }
+  
+  export default resumeData
